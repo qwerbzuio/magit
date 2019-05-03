acce9baeadc64c65d8e029306210b843037c4c9b
Benjamin Motz <benjamin.motz@mailbox.org>
2019-05-03 18:58:13 +0200

    Implement feature to copy only added/removed lines

--- lisp/magit-extras.el
+++ lisp/magit-extras.el
@@ -543,6 +543,34 @@ the minibuffer too."
 (define-key git-commit-mode-map
   (kbd "C-c C-w") 'magit-pop-revision-stack)
 
+(defun magit-filter-hunk-substring (result)
+  "Strip the outer diff marker column. With a positive/negative
+numeric argument, strip the outer diff marker column and copy
+only added/removed lines, as well as unchanged lines."
+  (let ((result result))
+    (when current-prefix-arg
+      (let* ((magit-diff-sign-regexp
+              (cond
+               ((listp current-prefix-arg) "-\\|\\+")
+               ((< (prefix-numeric-value current-prefix-arg) 0) "-")
+               (t "\\+")))
+             (magit-diff-filter-func
+              (lambda (str)
+                (if (listp current-prefix-arg) t
+                  (if (string-match-p
+                       (format "^\\(%s\\| \\)" magit-diff-sign-regexp) str)
+                      t
+                    nil)))))
+        (setq result
+              (mapconcat
+               (lambda (str)
+                 (replace-regexp-in-string
+                  (format "^\\(%s\\| \\)" magit-diff-sign-regexp) "" str))
+               (-filter (apply-partially magit-diff-filter-func)
+                        (split-string result "\n"))
+               "\n"))))
+    result))
+
 ;;;###autoload
 (defun magit-copy-section-value ()
   "Save the value of the current section for later use.
@@ -558,16 +586,18 @@ argument is used, then save the revision at its tip to the
 
 When the region is active, then save that to the `kill-ring',
 like `kill-ring-save' would, instead of behaving as described
-above.  If a prefix argument is used and the region is within a
-hunk, strip the outer diff marker column."
+above.  If a universal prefix argument is used and the region is
+within a hunk, strip the outer diff marker column. With a
+positive/negative numeric argument, strip the outer diff marker
+column and copy only added/removed lines, as well as unchanged
+lines."
   (interactive)
   (cond
    ((and current-prefix-arg
          (magit-section-internal-region-p)
          (magit-section-match 'hunk))
     (deactivate-mark)
-    (kill-new (replace-regexp-in-string
-               "^[ \\+\\-]" ""
+    (kill-new (magit-filter-hunk-substring
                (buffer-substring-no-properties
                 (region-beginning) (region-end)))))
    ((use-region-p)
